ace86de6b781216900446c0b087502ddf51e0041
Jake Hansen <jacobdhansen@unomaha.edu>
2020-04-21 22:28:58 -0500

    Add Runner which will be the main runner class

---
+++ src/java/simulator/Runner.java
@@ -0,0 +1,22 @@
+package java.simulator;
+
+import java.util.ArrayList;
+
+/**
+ * Runs the simulation.
+ */
+public class Runner {
+    public static void main (String[] args) {
+        System.out.println("Welcome to virtual memory simulator");
+    }
+
+    /**
+     * Validates command line arguments to ensure they are within the expected range(s).
+     * @param args Command line arguments to parse.
+     * @return True if arguments are valid, false otherwise.
+     */
+    private boolean validateArguments(String[] args) {
+        boolean argumentsValid = true;
+        return argumentsValid;
+    }
+}
