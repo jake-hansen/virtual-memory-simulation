4e33978faf58cadb985d50428e2bbf8fe3ea911b
Jake Hansen <jacobdhansen@unomaha.edu>
2020-04-21 23:32:53 -0500

    Add FileParser which will parse input file

---
+++ src/java/util/FileParser.java
@@ -0,0 +1,26 @@
+package java.util;
+
+import java.io.File;
+
+/**
+ * Utilities for parsing input file.
+ */
+public class FileParser {
+
+    /**
+     * File to parse.
+     */
+    private File inputFile;
+
+    /**
+     * Default constructor sets input file.
+     * @param inputFile File to parse.
+     */
+    public FileParser(File inputFile) {
+        this.inputFile = inputFile;
+    }
+
+    public void parseFile() {
+        
+    }
+}
