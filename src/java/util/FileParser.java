a953c2f8aecf70f0ce9eeea686d92f331fe08e8b
Jake Hansen <jacobdhansen@unomaha.edu>
2020-04-25 14:32:34 -0500

    Add start to parsing file

--- src/java/util/FileParser.java
+++ src/java/util/FileParser.java
@@ -1,26 +1,39 @@
 package java.util;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.model.MemoryMap;
 
-/**
- * Utilities for parsing input file.
- */
+/** Utilities for parsing input file. **/
 public class FileParser {
 
-    /**
-     * File to parse.
-     */
+    /** File to parse. **/
     private File inputFile;
 
     /**
      * Default constructor sets input file.
+     *
      * @param inputFile File to parse.
      */
     public FileParser(File inputFile) {
         this.inputFile = inputFile;
     }
 
-    public void parseFile() {
-        
+    /**
+     * Parses file for memory mappings. File needs to have each memory mapping
+     * on a separate line. Each line should be of the format "PROCESS# FRAME#"
+     * where PROCESS# and FRAME# are the process number and frame number,
+     * respectively.
+     *
+     * @return LinkedList<MemoryMap> containing memory mapping from file.
+     * @throws FileNotFoundException Thrown when file is not found.
+     */
+    public LinkedList<MemoryMap> parseFile() throws FileNotFoundException {
+        LinkedList<MemoryMap> returnMap = new LinkedList<>();
+        BufferedReader br = new BufferedReader(new FileReader(this.inputFile));
+
+        return returnMap;
     }
 }
