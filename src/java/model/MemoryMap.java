1bba12a19e17406681f42328021dc723a8702c52
Jake Hansen <jacobdhansen@unomaha.edu>
2020-04-25 14:24:35 -0500

    Add MemoryMap to represent processes and frames

---
+++ src/java/model/MemoryMap.java
@@ -0,0 +1,42 @@
+package java.model;
+
+/**
+ * Represents a mapping of process numbers to frame numbers.
+ */
+public class MemoryMap {
+
+    /** Represents process number. **/
+    private final int processNumber;
+
+    /** Represents frame number. **/
+    private final int frameNumber;
+
+    /**
+     * Default constructor sets processNumber and frameNumber to specified values.
+     *
+     * @param processNumber Process number.
+     * @param frameNumber Frame number.
+     */
+    public MemoryMap(int processNumber, int frameNumber) {
+        this.processNumber = processNumber;
+        this.frameNumber = frameNumber;
+    }
+
+    /**
+     * Gets the process number.
+     *
+     * @return Int representing the process number.
+     */
+    public int getProcessNumber() {
+        return processNumber;
+    }
+
+    /**
+     * Gets the frame number.
+     *
+     * @return Int representing the frame number.
+     */
+    public int getFrameNumber() {
+        return frameNumber;
+    }
+}
