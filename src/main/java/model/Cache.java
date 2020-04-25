2d13133d04d1881a7de470f8dba946458cc03645
Jake Hansen <jacobdhansen@unomaha.edu>
2020-04-25 17:48:51 -0500

    Add documentation for Cache.java

--- src/main/java/model/Cache.java
+++ src/main/java/model/Cache.java
@@ -5,19 +5,33 @@ import java.util.Queue;
 
 public class Cache {
 
-    private Queue<MemoryMap> queue;
+    /** Queue to hold MemoryMaps. **/
+    private final Queue<MemoryMap> queue;
 
+    /** Total number of cache hits. **/
     private int totalHits = 0;
 
+    /** Total number of cache misses. **/
     private int totalMisses = 0;
 
-    private int size;
+    /** Size limit for cache. **/
+    private final int size;
 
+    /**
+     * Constructor for cache that takes size limit for cache as parameter.
+     *
+     * @param size Size limit for cache.
+     */
     public Cache(int size) {
         this.size = size;
         queue = new LinkedList<>();
     }
 
+    /**
+     * Processes a request for a frame. Uses LRU replacment policy.
+     *
+     * @param mm MemoryMap to process.
+     */
     public void process(MemoryMap mm) {
         if (queue.contains(mm)) {
             queue.remove(mm);
@@ -31,6 +45,11 @@ public class Cache {
         queue.add(mm);
     }
 
+    /**
+     * Calculate hit ratio.
+     *
+     * @return Hit ratio.
+     */
     public double getRatio() {
         return (double) totalHits / ((double) totalMisses + (double) totalHits);
     }
